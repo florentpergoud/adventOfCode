6cf62f48a59ad70c7357b26aceabca4e5447eb67
Florent Pergoud <florent@cit.io>
2021-12-01 10:35:12 +0100

    feat: 🎸 prepare for 2021

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,37 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true,
   },
-  extends: ["airbnb-base"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:react-hooks/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 12,
+    sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
-  rules: {},
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/prop-types": "off",
+    "react/display-name": "warn",
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+    "arrow-parens": [2, "as-needed"],
+    "object-shorthand": ["error", "always"],
+    "no-implicit-globals": "error",
+    "no-restricted-globals": ["error", "name"],
+  },
+  settings: {
+    react: {
+      version: "latest",
+    },
+  },
 };
